285525e180da757517ffb1b158c6b697e2577316
Ingo Fischer <github@fischer-ka.de>
2022-11-07 09:15:54 +0100

    Update DescriptorCluster specs

--- src/matter/cluster/DescriptorCluster.ts
+++ src/matter/cluster/DescriptorCluster.ts
@@ -4,25 +4,51 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ArrayT, Field, ObjectT, UInt16T, UInt32T } from "../../codec/TlvObjectCodec";
+import { ArrayT, Field, ObjectT, UInt16T, Bound } from "../../codec/TlvObjectCodec";
 import { Attribute, Cluster } from "./Cluster";
+import { DeviceTypeIdT } from "../common/DeviceTypeId";
+import { ClusterIdT } from "../common/ClusterId";
+import { EndpointNumberT } from "../common/EndpointNumber";
+import { MatterCoreSpecificationV1_0 } from "../../Specifications";
 
+/**
+ * Provides information about endpoint conformance to a release of a device type definition.
+ *
+ * @see {@link MatterCoreSpecificationV1_0} § 9.5.5.1
+ */
 const DeviceTypeT = ObjectT({
-    type: Field(0, UInt32T), /* type: devtype-id */
-    revision: Field(1, UInt16T),
+    /** Indicates the device type definition */
+    type: Field(0, DeviceTypeIdT),
+    /** Indicates the implemented revision of the device type definition */
+    revision: Field(1, Bound(UInt16T, {min: 1})),
 });
 
 /**
- * The Descriptor Cluster is meant to replace the support from the Zigbee Device Object (ZDO) for describing a node,
+ * This cluster describes an endpoint instance on the node, independently of other endpoints, but also
+ * allows composition of endpoints to conform to complex device type patterns.
+ * This Cluster is also meant to replace the support from the Zigbee Device Object (ZDO) for describing a node,
  * its endpoints and clusters.
+ *
+ * clusterRevision: 1
+ *
+ * @see {@link MatterCoreSpecificationV1_0} § 9.5
  */
 export const DescriptorCluster = Cluster({
     id: 0x1d,
     name: "Descriptor",
+
+    /** @see {@link MatterCoreSpecificationV1_0} § 9.5.4 */
     attributes: {
+        /** List of device types and corresponding revisions declaring endpoint conformance. */
         deviceTypeList: Attribute(0, ArrayT(DeviceTypeT, { minLength: 1 })),
-        serverList: Attribute(1, ArrayT(UInt32T), { default: [] }), /* type: list[cluster-id] */
-        clientList: Attribute(3, ArrayT(UInt32T), { default: [] }), /* type: list[cluster-id] */
-        partsList: Attribute(4, ArrayT(UInt16T), { default: [] }), /* type: list[endpoint-no] */
+        /** List containing each cluster ID for the server clusters present on the endpoint instance. */
+        serverList: Attribute(1, ArrayT(ClusterIdT), { default: [] }),
+        /** List containing each cluster ID for the client clusters present on the endpoint instance. */
+        clientList: Attribute(3, ArrayT(ClusterIdT), { default: [] }),
+        /**
+         * This indicates composition of the device type instance. Device type instance composition SHALL
+         * include the endpoints in this list.
+         */
+        partsList: Attribute(4, ArrayT(EndpointNumberT), { default: [] }),
     },
 });

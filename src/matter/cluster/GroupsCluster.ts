ec212879420b1638c682c7f029e715c1bd0ff139
Ingo Fischer <github@fischer-ka.de>
2022-11-07 09:29:22 +0100

    Add GroupsCluster specs

---
+++ src/matter/cluster/GroupsCluster.ts
@@ -0,0 +1,174 @@
+/**
+ * @license
+ * Copyright 2022 The node-matter Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+    Field,
+    ObjectT,
+    StringT,
+    UInt8T,
+    ArrayT,
+    EnumT
+} from "../../codec/TlvObjectCodec";
+import { Attribute, Cluster, Command, NoArgumentsT, NoResponseT } from "./Cluster";
+import { StatusCode } from "../interaction/InteractionMessages";
+import { GroupIdT } from "../common/GroupId";
+import { MatterApplicationClusterSpecificationV1_0 } from "../../Specifications";
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.1 */
+const AddGroupRequestT = ObjectT({
+    groupId: Field(0, GroupIdT), /* min: 1 */
+    groupName: Field(1, StringT({ maxLength: 16 })),
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.7 */
+const AddGroupResponseT = ObjectT({
+    status: Field(0, EnumT<StatusCode>()),
+    groupId: Field(1, GroupIdT), /* type: min: 1 */
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.2 */
+const ViewGroupRequestT = ObjectT({
+    groupId: Field(0, GroupIdT), /* type: min: 1 */
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.8 */
+const ViewGroupResponseT = ObjectT({
+    status: Field(0, EnumT<StatusCode>()),
+    groupId: Field(1, GroupIdT), /* min: 1 */
+    groupName: Field(2, StringT( { maxLength: 16 })),
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.3 */
+const GetGroupMembershipRequestT = ObjectT({
+    groupList: Field(0, ArrayT(GroupIdT)), /* groupId min: 1 */
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.9 */
+const GetGroupMembershipResponseT = ObjectT({
+    /** contain the remaining capacity of the Group Table of the node. */
+    capacity: Field(0, UInt8T), /* nullable: true */
+    groupList: Field(1, ArrayT(GroupIdT)),
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.4 */
+const RemoveGroupRequestT = ObjectT({
+    groupId: Field(0, GroupIdT), /* min: 1 */
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.10 */
+const RemoveGroupResponseT = ObjectT({
+    status: Field(0, EnumT<StatusCode>()),
+    groupId: Field(1, GroupIdT), /* min: 1 */
+});
+
+
+/**
+ * Formally not defined in specs state:
+ * If the RemoveAllGroups command was received as unicast and a response is not
+ * suppressed, the server SHALL generate a response with the Status field set to SUCCESS.
+ *
+ * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.5.1
+ */
+const RemoveAllGroupResponseT = ObjectT({
+    status: Field(0, EnumT<StatusCode>()),
+});
+
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.6 */
+const AddGroupIfIdentifyingRequestT = ObjectT({
+    groupId: Field(0, GroupIdT), /* min: 1 */
+    groupName: Field(1, StringT( { maxLength: 16 })),
+});
+
+
+/*
+  TODO
+  * Feature map:
+    * Bit 0: Group Names - The ability to store a name for a group.
+  * Dependencies:
+    * For correct operation of the AddGroupIfIdentifying command, any endpoint
+      that supports the Groups server cluster SHALL also support the Identify
+      server cluster.
+    * For RemoveGroup: Additionally, if the Scenes cluster is supported on the same endpoint,
+      scenes associated with the indicated group SHALL be removed on that endpoint.
+    * For RemoveAllGroups: Additionally, if the Scenes cluster is supported on the same
+      endpoint, all scenes, except for scenes associated with group ID 0, SHALL be removed
+      on that endpoint.
+ */
+
+/**
+ * The Groups cluster manages, per endpoint, the content of the node-wide Group
+ * Table that is part of the underlying interaction layer.
+ *
+ * clusterRevision: 4
+ * featureMap: 1 - Bit 0 (Group Names) is set
+ *
+ * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3
+ */
+export const GroupsCluster = Cluster({
+    id: 0x04,
+    name: "Groups",
+
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.6 */
+    attributes: {
+        /**
+         * This attribute provides legacy, read-only access to whether the Group
+         * Names feature is supported. The most significant bit, bit 7, SHALL be
+         * equal to bit 0 of the FeatureMap attribute. All other bits SHALL be 0.
+         *
+         * TODO because we support group names we need to set bit 7 to 1, rest is 0
+         */
+        nameSupport: Attribute(0, UInt8T, { default: 0x80 }), /* TODO BitMap!! */
+    },
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7 */
+    commands: {
+        /**
+         * The AddGroup command allows a client to add group membership in a particular group for the server endpoint.
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.1
+         */
+        addGroup: Command(0, AddGroupRequestT, 0, AddGroupResponseT),
+        /**
+         * The ViewGroup command allows a client to request that the server responds with a ViewGroupResponse command
+         * containing the name string for a particular group.
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.2
+         */
+        viewGroup: Command(1, ViewGroupRequestT, 0, ViewGroupResponseT),
+        /**
+         * The GetGroupMembership command allows a client to inquire about the group membership of the server endpoint,
+         * in a number of ways.
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.3
+         */
+        getGroupMembership: Command(2, GetGroupMembershipRequestT, 2, GetGroupMembershipResponseT),
+        /**
+         * The RemoveGroup command allows a client to request that the server removes the membership for the server
+         * endpoint, if any, in a particular group.
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.4
+         */
+        removeGroup: Command(3, RemoveGroupRequestT, 3, RemoveGroupResponseT),
+        /**
+         * The RemoveAllGroups command allows a client to direct the server to remove all group associations for the
+         * server endpoint.
+         *
+         * TODO: According to specs the response might be suppressed:
+         * If the RemoveAllGroups command was received as unicast and a response is not suppressed, the server
+         * SHALL generate a response with the Status field set to SUCCESS.
+         * Else potentially no response?
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.5
+         */
+        removeAllGroups: Command(4, NoArgumentsT, 4, RemoveAllGroupResponseT),
+        /**
+         * The AddGroupIfIdentifying command allows a client to add group membership in a particular group for the
+         * server endpoint, on condition that the endpoint is identifying itself.
+         *
+         * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.3.7.6
+         */
+        addGroupIfIdentifying: Command(5, AddGroupIfIdentifyingRequestT, 5, NoResponseT),
+    }
+});

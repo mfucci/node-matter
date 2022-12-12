646aff17d42d21ec29eed64f92bbb568bb61cb40
Ingo Fischer <github@fischer-ka.de>
2022-12-12 09:33:40 +0100

    adjustments for addGroupIfIdentifying

--- src/matter/cluster/server/GroupsServer.ts
+++ src/matter/cluster/server/GroupsServer.ts
@@ -68,7 +68,7 @@ export const GroupsClusterHandler: () => ClusterServerHandlers<typeof GroupsClus
             // TODO Later:
             //  Zigbee: If the total number of groups will cause the maximum payload length of a frame to be exceeded,
             //  then the GroupList field SHALL contain only as many groups as will fit.
-    
+
             const allGroupsList = Array.from(clusterGroups.keys());
             const capacity = allGroupsList.length < 0xff ? 0xFF - allGroupsList.length : 0xfe;
             if (groupList.length === 0) {
@@ -96,10 +96,10 @@ export const GroupsClusterHandler: () => ClusterServerHandlers<typeof GroupsClus
 
         removeAllGroups: async () => {
             clusterGroups.clear();
-    
+
             // TODO Additionally, if the Scenes cluster is supported on the same endpoint, all scenes, except for scenes
             //      associated with group ID 0, SHALL be removed on that endpoint.
-    
+
             // TODO If the RemoveAllGroups command was received as unicast and a response is not suppressed ... return Success
             return {status: StatusCode.Success};
         },
@@ -108,11 +108,12 @@ export const GroupsClusterHandler: () => ClusterServerHandlers<typeof GroupsClus
             // TODO The server verifies that it is currently identifying itself. If the server it not currently identifying
             //      itself, the status SHALL be SUCCESS
             // return {status: StatusCode.Success, groupId};
-    
+
             // TODO If the AddGroupIfIdentifying command was received as unicast and the evaluated status is not SUCCESS, or
             //      if the AddGroupIfIdentifying command was received as unicast and the evaluated status is SUCCESS and a
             //      response is not suppressed, the server SHALL generate a response with the Status field set to the
             //      evaluated status.
+            return this.addGroup({request: {groupId, groupName}});
         },
     }
 };

7a730ba3b265607069671881e280668ac6897714
Ingo Fischer <github@fischer-ka.de>
2023-02-01 12:36:19 +0100

    Fix IlluminanceMeasurement cluster definition

--- src/matter/cluster/IlluminanceMeasurementCluster.ts
+++ src/matter/cluster/IlluminanceMeasurementCluster.ts
@@ -10,22 +10,30 @@ import { BitFlag, MatterApplicationClusterSpecificationV1_0, TlvBitmap, TlvInt16
 /**
  * Attributes and commands for Illuminance Measurement.
  *
- * @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3
+ * @see {@link MatterApplicationClusterSpecificationV1_0} § 2.2
  */
 export const IlluminanceMeasurementCluster = Cluster({
     id: 0x0400,
     name: "IlluminanceMeasurement",
-    revision: 4,
+    revision: 3,
     features: { }, // no features
 
-    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3.4 */
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.2.5 */
     attributes: {
-       measuredValue: Attribute(0, TlvUInt16.bound({ min: 0, max: 65534 }), { default: 1 }),
-       minMeasuredValue: Attribute(1, TlvUInt16.bound({ min: 0, max: 65534 }), { default: 27315 }),
-       maxMeasuredValue: Attribute(2, TlvUInt16.bound({ min: 0, max: 65534 }), { default: 32767 }),
-       tolerance: Attribute(3, TlvUInt16.bound({ min: 0, max: 2048 }), { default: 1 }),
+       /** Represents the illuminance in Lux (symbol lx): MeasuredValue = 10,000 x log10(illuminance) + 1 */
+       measuredValue: Attribute(0, TlvNullable(TlvUInt16)), { default: 0 }),
+       
+       /** Indicates the minimum value of MeasuredValue that can be measured. */
+       minMeasuredValue: Attribute(1, TlvNullable(TlvUInt16.bound({ min: 1 }))),
+       
+       /** Indicates the maximum value of MeasuredValue that can be measured. */
+       maxMeasuredValue: Attribute(2, TlvNullable(TlvUInt16.bound({ max: 65534 }))),
+       tolerance: OptionalAttribute(3, TlvUInt16.bound({ min: 0, max: 2048 })),
+       
+       /** Specifies the electronic type of the light sensor. */
+       lightSensorType: OptionalAttribute(4, TlvNullable(TlvUInt8)), // only values null, 0, 1 and 0x40 to 0xfe are allowed
     },
 
-    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3.5 */
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.2.6 */
     commands: { },
 });

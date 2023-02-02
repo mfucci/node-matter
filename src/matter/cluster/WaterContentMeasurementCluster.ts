f9e9dba4b520e9aa522cd6d226bd578ec019daf0
Jim Buzbee <jimbuzbee@gmail.com>
2023-02-01 18:45:29 -0700

    Clean up doc references, min, max and default values. Create three different clusters, Relative Humidity Measurement, Leaf Wetness Measurement, Soil Moisture Measurement. Denote optional attributes
    Clean up doc references. Fix min, max, and default values. Add three
    diffferent clusters: Relative Humidity Measurement, Leaf Wetness
    Measurement, Soil Moisture Measurement. Denote optional attributes

--- src/matter/cluster/WaterContentMeasurementCluster.ts
+++ src/matter/cluster/WaterContentMeasurementCluster.ts
@@ -4,28 +4,86 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Attribute, Cluster, Command, TlvNoArguments, TlvNoResponse } from "./Cluster";
+import { Attribute, OptionalAttribute, Cluster, Command, TlvNoArguments, TlvNoResponse } from "./Cluster";
 import { BitFlag, MatterApplicationClusterSpecificationV1_0, TlvBitmap, TlvInt16, TlvUInt16, TlvEnum, TlvField, TlvNullable, TlvObject, TlvSchema, TlvUInt8 } from "@project-chip/matter.js";
 
 /**
  * Attributes and commands for WaterContent Measurement.
  *
- * @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3
+ * @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6
  */
-export const WaterContentMeasurementCluster = Cluster({
+/** Percentage of water in the air */
+export const RelativeHumidityCluster = Cluster({
     id: 0x0405,
-    name: "WaterContentMeasurement",
-    revision: 4,
+    name: "RelativeHumidityMeasurement",
+    revision: 3,
     features: { }, // no features
 
-    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3.4 */
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.3 */
     attributes: {
-       measuredValue: Attribute(0, TlvUInt16.bound({ min: 0, max: 10000 }), { default: 1 }),
-       minMeasuredValue: Attribute(1, TlvUInt16.bound({ min: 0 , max: 10000-1-1 }), { default: 27315 }),
-       maxMeasuredValue: Attribute(2, TlvUInt16.bound({ min: 0+1, max: 10000 }), { default: 32767 }),
-       tolerance: Attribute(3, TlvUInt16.bound({ min: 0, max: 2048 }), { default: 1 }),
+       /** MeasuredValue represents the water content in % as follows: MeasuredValue = 100 x water content */
+       measuredValue: Attribute(0, TlvNullable(TlvUInt16.bound({ min: 0, max: 10000 }))),
+
+       /** Indicates the minimum value of MeasuredValue that can be measured. */
+       minMeasuredValue: Attribute(1, TlvNullable(TlvUInt16.bound({ min: 0 }))),
+
+       /** Indicates the maximum value of MeasuredValue that can be measured. */
+       maxMeasuredValue: Attribute(2, TlvNullable(TlvUInt16.bound({ max: 10000 }))),
+
+       tolerance: OptionalAttribute(3, TlvUInt16.bound({ min: 0, max: 2048 })),
     },
 
-    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.3.5 */
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.5 */
     commands: { },
 });
+
+/** Percentage of water on the leaves of plants */
+export const LeafWetnessMeasurementCluster = Cluster({
+    id: 0x0407,
+    name: "LeafWetnessMeasurement",
+    revision: 3,
+    features: { }, // no features
+
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.3 */
+    attributes: {
+       /** MeasuredValue represents the water content in % as follows: MeasuredValue = 100 x water content */
+       measuredValue: Attribute(0, TlvNullable(TlvUInt16.bound({ min: 0, max: 10000 }))),
+
+       /** Indicates the minimum value of MeasuredValue that can be measured. */
+       minMeasuredValue: Attribute(1, TlvNullable(TlvUInt16.bound({ min: 0 }))),
+
+       /** Indicates the maximum value of MeasuredValue that can be measured. */
+       maxMeasuredValue: Attribute(2, TlvNullable(TlvUInt16.bound({ max: 10000 }))),
+
+       tolerance: OptionalAttribute(3, TlvUInt16.bound({ min: 0, max: 2048 })),
+    },
+
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.5 */
+    commands: { },
+});
+
+/** Percentage of water in the soil */
+export const SoilMoistureMeasurementCluster = Cluster({
+    id: 0x0408, 
+    name: "SoilMoistureMeasurement",
+    revision: 3,
+    features: { }, // no features
+
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.3 */
+    attributes: {
+       /** MeasuredValue represents the water content in % as follows: MeasuredValue = 100 x water content */
+       measuredValue: Attribute(0, TlvNullable(TlvUInt16.bound({ min: 0, max: 10000 }))),
+
+       /** Indicates the minimum value of MeasuredValue that can be measured. */
+       minMeasuredValue: Attribute(1, TlvNullable(TlvUInt16.bound({ min: 0 }))),
+
+       /** Indicates the maximum value of MeasuredValue that can be measured. */
+       maxMeasuredValue: Attribute(2, TlvNullable(TlvUInt16.bound({ max: 10000 }))),
+
+       tolerance: OptionalAttribute(3, TlvUInt16.bound({ min: 0, max: 2048 })),
+    },
+
+    /** @see {@link MatterApplicationClusterSpecificationV1_0} § 2.6.5 */
+    commands: { },
+});
+

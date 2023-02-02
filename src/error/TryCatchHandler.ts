fbcc6b16d853d4258cdcb69455123961058740f7
Ingo Fischer <github@fischer-ka.de>
2023-02-02 10:23:07 +0100

    Add some JSDoc

--- src/error/TryCatchHandler.ts
+++ src/error/TryCatchHandler.ts
@@ -6,6 +6,15 @@
 
 type ErrorHandler<T> = (error: Error) => T | undefined;
 
+/**
+ * Try to execute the code block and catch the error if it is of the given type.
+ * If the error is of the given type, the fallback value or the result of the function is returned.
+ * If the function returns undefined or the error type do not match, the error is normally thrown and not handled.
+ *
+ * @param codeBlock Code block to execute
+ * @param errorType Errortype to catch and handle
+ * @param fallbackValueOrFunction Fallback value or function to compute the fallback value
+ */
 export function tryCatch<T>(codeBlock: () => T, errorType: {new (...args: any[]): Error}, fallbackValueOrFunction: ErrorHandler<T> | T): T {
     try {
         return codeBlock();
@@ -22,6 +31,15 @@ export function tryCatch<T>(codeBlock: () => T, errorType: {new (...args: any[])
     }
 }
 
+/**
+ * Try to execute the async code block and catch the error if it is of the given type.
+ * If the error is of the given type, the fallback value or the result of the function is returned.
+ * If the function returns undefined or the error type do not match, the error is normally thrown and not handled.
+ *
+ * @param codeBlock Async code block to execute
+ * @param errorType Errortype to catch and handle
+ * @param fallbackValueOrFunction Fallback value or function to compute the fallback value
+ */
 export async function tryCatchAsync<T>(codeBlock: () => Promise<T>, errorType: {new (...args: any[]): Error}, fallbackValueOrFunction: ErrorHandler<T> | T): Promise<T> {
     try {
         return await codeBlock();

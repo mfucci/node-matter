6ee01c53ac415fb5625687138bf7323d5e7a7865
Ingo Fischer <github@fischer-ka.de>
2023-02-19 15:24:06 +0100

    Initialize Secure Case Session earlier

--- src/matter/session/secure/CaseServer.ts
+++ src/matter/session/secure/CaseServer.ts
@@ -45,10 +45,16 @@ export class CaseServer implements ProtocolHandler<MatterDevice> {
         // Try to resume a previous session
         const resumptionId = Crypto.getRandomData(16);
         let resumptionRecord;
+
+        // We try to resume the session
         if (peerResumptionId !== undefined && peerResumeMic !== undefined && (resumptionRecord = server.findResumptionRecordById(peerResumptionId)) !== undefined) {
             const { sharedSecret, fabric, peerNodeId } = resumptionRecord;
             const peerResumeKey = await Crypto.hkdf(sharedSecret, ByteArray.concat(peerRandom, peerResumptionId), KDFSR1_KEY_INFO);
             Crypto.decrypt(peerResumeKey, peerResumeMic, RESUME1_MIC_NONCE);
+
+            // All good! Create secure session
+            const secureSessionSalt = ByteArray.concat(peerRandom, peerResumptionId);
+            const secureSession = await server.createSecureSession(sessionId, fabric, peerNodeId, peerSessionId, sharedSecret, secureSessionSalt, false, true, mrpParams?.idleRetransTimeoutMs, mrpParams?.activeRetransTimeoutMs);
 
             // Generate sigma 2 resume
             const resumeSalt = ByteArray.concat(peerRandom, resumptionId);
@@ -56,9 +62,6 @@ export class CaseServer implements ProtocolHandler<MatterDevice> {
             const resumeMic = Crypto.encrypt(resumeKey, new ByteArray(0), RESUME2_MIC_NONCE);
             await messenger.sendSigma2Resume({ resumptionId, resumeMic, sessionId });
 
-            // All good! Create secure session
-            const secureSessionSalt = ByteArray.concat(peerRandom, peerResumptionId);
-            const secureSession = await server.createSecureSession(sessionId, fabric, peerNodeId, peerSessionId, sharedSecret, secureSessionSalt, false, true, mrpParams?.idleRetransTimeoutMs, mrpParams?.activeRetransTimeoutMs);
             logger.info(`Case server: session ${secureSession.getId()} resumed with ${messenger.getChannelName()}`);
             resumptionRecord.resumptionId = resumptionId; /* Update the ID */
 

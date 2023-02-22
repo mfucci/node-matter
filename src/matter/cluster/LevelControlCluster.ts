f7cfaf433d68fce5aa3a72be95ae39bccc7a156c
Jim Buzbee <jimbuzbee@gmail.com>
2023-02-22 08:29:27 -0700

    Update with changes to address review by @mfucci

--- src/matter/cluster/LevelControlCluster.ts
+++ src/matter/cluster/LevelControlCluster.ts
@@ -4,150 +4,152 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { OptionalAttribute, WritableAttribute, OptionalWritableAttribute, Attribute, Cluster, OptionalCommand, TlvNoResponse } from "./Cluster";
+import { OptionalAttribute, WritableAttribute, OptionalWritableAttribute, Attribute, Cluster, Command, OptionalCommand, TlvNoResponse } from "./Cluster";
 import { BitFlag, MatterApplicationClusterSpecificationV1_0, TlvBitmap, TlvEnum, TlvField, TlvNullable, TlvObject, TlvUInt16, TlvUInt8 } from "@project-chip/matter.js";
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.5.8 */
-export const OptionsBitmap = {
-    /** Command execution control - see 1.6.5.8.1. */
-    ExecuteIfOff: BitFlag(0), // TODO default: true.
+const optionsBitmap = TlvBitmap(TlvUInt8, {
+    /** Dependency on On/Off cluster. */
+    executeIfOff: BitFlag(0), // TODO default: true.
 
-    /** level effect on color - see 1.6.5.8.2 */
-    CoupleColorTempToLevel: BitFlag(1),
-};
+    /** Level effect on color. */
+    coupleColorTempToLevel: BitFlag(1),
+});
 
 /** @see {@link MatterCoreSpecificationV1_0} § 1.6.6.1 */
-const  MoveToLevelCommand = TlvObject({
-    level: TlvField(0, TlvUInt8.bound({min:0, max:254})),
+const MoveToLevelCommandRequest = TlvObject({
+    level: TlvField(0, TlvUInt8.bound({max:254})),
     transitionTime: TlvField(1, TlvNullable(TlvUInt16)),
-    optionsMask: TlvField(2, TlvBitmap(TlvUInt8, OptionsBitmap)),
-    optionsOverride: TlvField(3, TlvBitmap(TlvUInt8, OptionsBitmap)), // TODO: 0 Default - all optionsOverride
+    optionsMask: TlvField(2, optionsBitmap),
+    optionsOverride: TlvField(3, optionsBitmap), // TODO: 0 Default - all optionsOverride
 });
 
 /** @see {@link MatterCoreSpecificationV1_0} § 1.6.6.2 */
-const  MoveCommand = TlvObject({
+const MoveCommandRequest = TlvObject({
     moveMode: TlvField(0, TlvEnum<MoveMode>()),
     rate: TlvField(1, TlvNullable(TlvUInt8)),
-    optionsMask: TlvField(2, TlvBitmap(TlvUInt8, OptionsBitmap)),
-    optionsOverride: TlvField(3, TlvBitmap(TlvUInt8, OptionsBitmap)),
+    optionsMask: TlvField(2, optionsBitmap),
+    optionsOverride: TlvField(3, optionsBitmap),
 });
 
 /** @see {@link MatterCoreSpecificationV1_0} § 1.6.6.3 */
-const  StepCommand = TlvObject({
+const StepCommandRequest = TlvObject({
     stepMode: TlvField(0, TlvEnum<StepMode>()),
     stepSize: TlvField(1, TlvUInt8),
     transitionTime: TlvField(2, TlvNullable(TlvUInt16)),
-    optionsMask: TlvField(3, TlvBitmap(TlvUInt8, OptionsBitmap)),
-    optionsOverride: TlvField(4, TlvBitmap(TlvUInt8, OptionsBitmap)),
+    optionsMask: TlvField(3, optionsBitmap),
+    optionsOverride: TlvField(4, optionsBitmap),
 });
 
 /** @see {@link MatterCoreSpecificationV1_0} § 1.6.6.4 */
-const  StopCommand = TlvObject({
-    optionsMask: TlvField(0, TlvBitmap(TlvUInt8, OptionsBitmap)),
-    optionsOverride: TlvField(1, TlvBitmap(TlvUInt8, OptionsBitmap)),
+const StopCommandRequest = TlvObject({
+    optionsMask: TlvField(0, optionsBitmap),
+    optionsOverride: TlvField(1, optionsBitmap),
 });
 
 /** @see {@link MatterCoreSpecificationV1_0} § 1.6.6.5  */
-const MoveToClosestFrequencyCommand = TlvObject({
-    frequency: TlvField(0, TlvUInt16 ), // FIXME - Default 0, how to set TlvField default?
+const MoveToClosestFrequencyCommandRequest = TlvObject({
+    frequency: TlvField(0, TlvUInt16), // TODO - Default 0, how to set TlvField default?
 })
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.4 */
 const features = {
     /** Dependency with the On/Off cluster */
-    OnOff: BitFlag(0), // TODO default: true.
+    onOffClusterDependency: BitFlag(0), // TODO default: true.
 
-    /** Behavior that supports lighting applications */
-    Lighting: BitFlag(1),
+    /** Behavior that supports lighting applications. */
+    lighting: BitFlag(1),
 
     /** Supports frequency attributes and behavior. */
-    Frequency: BitFlag(2)
+    supportFrequency: BitFlag(2)
 };
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.6.2.1 */
 const enum MoveMode {
-    up = 0x0,
-    down = 0x1,
+    Up = 0x0,
+    Down = 0x1,
 };
+
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.6.3 */
 const enum StepMode {
-    up = 0x0,
-    down = 0x1,
+    Up = 0x0,
+    Down = 0x1,
 };
 
- /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.5 */
+/** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.5 */
 const attributes = {
-    /** the current level of this device */
-    currentLevel: Attribute(0x0,  TlvNullable(TlvUInt8), {default: null}), 
+    /** Current level of this device. */
+    currentLevel: Attribute(0x0, TlvNullable(TlvUInt8), {default: null}), 
 
-    /** time until the current command is complete in 1/10ths of sec */
+    /** Time until the current command is complete in 1/10ths of sec. */
     remainingTime: OptionalAttribute(0x01, TlvUInt16, {default: 0}),
 
-    /** the minimum value of CurrentLevel  */
-    minLevel: OptionalAttribute(0x02, TlvUInt8.bound({ min: 0})),
+    /** Minimum value of {@link attributes.currentLevel}. */
+    minLevel: OptionalAttribute(0x02, TlvUInt8),
 
-    /** the maximum value of CurrentLevel */
-    maxLevel: Attribute(0x03, TlvUInt8.bound({ max: 254}), {default: 254}),
+    /** Maximum value of {@link attributes.currentLevel}. */
+    maxLevel: Attribute(0x03, TlvUInt8.bound({max: 254}), {default: 254}),
 
-    /** the frequency at which the device is at CurrentLevel */
+    /** Frequency at which the device is at CurrentLevel. */
     currentFrequency: OptionalAttribute(0x04, TlvUInt16, {default: 0}),
 
-    /** min value of CurrentFrequency capable of being assigned */
-    minFrequency: OptionalAttribute(0x05, TlvUInt16.bound({min: 0}), {default: 0}),
+    /** Min value of CurrentFrequency capable of being assigned. */
+    minFrequency: OptionalAttribute(0x05, TlvUInt16, {default: 0}),
 
-    /** max value of CurrentFrequency capable of being assigned */
+    /** Max value of CurrentFrequency capable of being assigned. */
     maxFrequency: OptionalAttribute(0x06, TlvUInt16, {default: 0}),
 
-    /** time to move to/from the target when On/Off received by On/Off cluster on the same endpoint */
-    onOffTransitionTime: OptionalAttribute(0x10, TlvUInt16, {default: 0}),
+    /** Time to move to/from the target when On/Off received by On/Off cluster on the same endpoint. */
+    onOffTransitionTime: OptionalWritableAttribute(0x10, TlvUInt16, {default: 0}),
 
-    /** CurrentLevel when OnOff attr of On/Off cluster on the same endpoint is TRUE */
-    onLevel: WritableAttribute(0x11,  TlvNullable(TlvUInt8), {default: null}),
+    /** CurrentLevel when OnOff attr of On/Off cluster on the same endpoint is TRUE. */
+    onLevel: WritableAttribute(0x11, TlvNullable(TlvUInt8), {default: null}),
 
-    /** time taken to move the current level from the min level to the max level */
+    /** Time taken to move the current level from the min level to the max level. */
     onTransitionTime: OptionalWritableAttribute(0x12, TlvNullable(TlvUInt16,), {default: null}),
 
-    /** time taken to move the current level from the max level to the minlevel  */
+    /** Time taken to move the current level from the max level to the minlevel. */
     offTransitionTime: OptionalWritableAttribute(0x13, TlvNullable(TlvUInt16), {default: null}),
 
-    /** default rate in units per second when a Move command has a null Rate */
+    /** Default rate in units per second when a Move command has a null rate. */
     defaultMoveRate: OptionalWritableAttribute(0x14, TlvNullable(TlvUInt8)),
 
-    options: Attribute(0x0f,  TlvBitmap(TlvUInt8, OptionsBitmap) ),
+    /** Determines the default behavior of some cluster commands. */
+    options: Attribute(0x0f, optionsBitmap),
 
-    startUpCurrentLevel: OptionalAttribute(0x4000, TlvNullable(TlvUInt8),),  
+    /** Desired startup level for a device when it is supplied with power. */
+    startUpCurrentLevel: OptionalWritableAttribute(0x4000, TlvNullable(TlvUInt8)),  
 };
-
-/* TODO - Implement all below and change from Optional to Mandatory */
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.6 */
 const commands = {
-    /** Move from the current level to the specified level */
-    moveToLevel: OptionalCommand(0x0, MoveToLevelCommand, 0x0, TlvNoResponse),
     
-    /** move from the current level up or down in a continuous fashion */
-    move: OptionalCommand(0x1, MoveCommand, 0x1, TlvNoResponse),
-
-    /** move from the current level up or down in a stepwise fashion */
-    step: OptionalCommand(0x2, StepCommand, 0x2, TlvNoResponse),
-
-    /** stop moving the current level */
-    stop: OptionalCommand(0x3, StopCommand, 0x3, TlvNoResponse),
-
-    /** same as moveToLevel, but change the on/off status of OnOff device on same endpoint */
-    moveToLevelWithOnOff: OptionalCommand( 0x4, MoveToLevelCommand, 0x4, TlvNoResponse ),
-
-     /** same as move, but change the on/off status of OnOff device on same endpoint */
-    moveWithOnOff: OptionalCommand(0x5, MoveCommand, 0x5,TlvNoResponse),    
+    /** Moves from the current level to the specified level.*/
+    moveToLevel: Command(0x0, MoveToLevelCommandRequest, 0x0, TlvNoResponse),
     
-    /** same as step, but change the on/off status of OnOff device on same endpoint */
-    stepWithOnOff: OptionalCommand(0x6, StepCommand, 0x6, TlvNoResponse),
+    /** Moves from the current level up or down in a continuous fashion. */
+    move: Command(0x1, MoveCommandRequest, 0x1, TlvNoResponse),
 
-    /** same as stop, but change the on/off status of OnOff device on same endpoint */
-    stopWithOnOff: OptionalCommand(0x7, StopCommand, 0x7, TlvNoResponse),
+    /** Moves from the current level up or down in a stepwise fashion. */
+    step: Command(0x2, StepCommandRequest, 0x2, TlvNoResponse),
 
-    /** change current frequency to the requested frequency, or to the closest frequency */
-    moveToClosestFrequency: OptionalCommand(0x8, MoveToClosestFrequencyCommand, 0x8, TlvNoResponse),
+    /** Stops moving the current level. */
+    stop: Command(0x3, StopCommandRequest, 0x3, TlvNoResponse),
+
+    /** Same as {@link commands.moveToLevel}, but change the status of OnOff device on same endpoint. */
+    moveToLevelWithOnOff: Command( 0x4, MoveToLevelCommandRequest, 0x4, TlvNoResponse),
+
+     /** Same as {@link commands.move}, but change the status of OnOff device on same endpoint. */
+    moveWithOnOff: Command(0x5, MoveCommandRequest, 0x5,TlvNoResponse),    
+    
+    /** Same as {@link commands.step}, but change the status of OnOff device on same endpoint. */
+    stepWithOnOff: Command(0x6, StepCommandRequest, 0x6, TlvNoResponse),
+
+    /** Same as {@link commands.stop}, but change the status of OnOff device on same endpoint. */
+    stopWithOnOff: Command(0x7, StopCommandRequest, 0x7, TlvNoResponse),
+
+    /** Changes current frequency to the requested frequency, or to the closest frequency. */
+    moveToClosestFrequency: OptionalCommand(0x8, MoveToClosestFrequencyCommandRequest, 0x8, TlvNoResponse),
 };
 
 /**
@@ -165,7 +167,7 @@ export const LevelControlCluster = Cluster({
  });
 
  /**
- * Attributes and commands for Pulse Width Moduation (Provisional)
+ * Attributes and commands for Pulse Width Modulation (Provisional)
  *
  * @see {@link MatterApplicationClusterSpecificationV1_0} § 1.6.3
  */
--- src/matter/cluster/server/LevelControlServer.ts
+++ src/matter/cluster/server/LevelControlServer.ts
@@ -11,18 +11,25 @@ import { UseOptionalAttributes } from "./ClusterServer";
 // TODO: Create temporary options based on mask and override. How to expose to user of the library?
 
 export const LevelControlClusterHandler: () => ClusterServerHandlers<typeof LevelControlCluster> = () => ({
-    moveToLevel: async ({ request:{level, transitionTime, optionsMask, optionsOverride}, attributes: { currentLevel } }) => { 
+    moveToLevel: async ({ request:{level}, attributes: {currentLevel} }) => { 
       currentLevel.set(level);
     }, 
 
     // TODO: How much this capability should be in the library vs. in the caller of the library? Doing the
     // move/step/stop (withOnOff) in the library would be making assumptions about the capabilities of the 
-    // underlying hardware. But how to expose these paramters to the caller of the library? Callback?
-    move: async ({ request:{moveMode, rate, optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
-    step: async ({ request:{stepMode, stepSize, optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
-    stop: async ({ request:{optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
+    // underlying hardware. But how to expose these paramters to the caller of the library? Callback with
+    // temporary options?
+    move: async ({ request:{}, attributes: {} }) => {
+      throw new Error("Not implementated")
+    },
+    step: async ({ request:{}, attributes: {} }) => {
+      throw new Error("Not implementated")
+    },
+    stop: async ({ request:{}, attributes: {} }) => {
+      throw new Error("Not implementated")
+    },
     
-    moveToLevelWithOnOff: async function ({ request: { level, optionsOverride }, attributes: { currentLevel} }) {
+    moveToLevelWithOnOff: async function ({ request: {level}, attributes: {currentLevel} }) {
       
       /* TODO: Need to be able to access or check existance of optional attributes
       if ( level <= minLevel.get() && associated onOff device on){
@@ -35,10 +42,19 @@ export const LevelControlClusterHandler: () => ClusterServerHandlers<typeof Leve
       currentLevel.set(level);
     },
     
-    moveWithOnOff: async ({ request:{moveMode, rate, optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
-    stepWithOnOff: async ({ request:{stepMode, stepSize, optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
-    stopWithOnOff: async ({ request:{optionsMask, optionsOverride}, attributes: { currentLevel } }) => {},
-    moveToClosestFrequency: async ({request:{frequency, }, attributes: { /* currentFrequency */ } }) => { }, 
+    moveWithOnOff: async ({ request:{}, attributes: { currentLevel } }) => {
+      throw new Error("Not implementated")
+    },
+    stepWithOnOff: async ({ request:{}, attributes: { currentLevel } }) => {
+      throw new Error("Not implementated")
+    },
+    stopWithOnOff: async ({ request:{}, attributes: { currentLevel } }) => {
+      throw new Error("Not implementated")
+    },
+    moveToClosestFrequency: async ({request:{}, attributes: { /* currentFrequency */ } }) => { 
     // TODO: How to access or check existance of optional attribute?
-    /* currentFrequency = frequency */
+
+      /* currentFrequency = frequency */
+      throw new Error("Not implementated")
+    }, 
 });
